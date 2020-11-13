89d7826713c51a9928a5ce168245355ed2b7096b
sliv2001 <ivan.sladkov@yandex.ru>
2020-11-13 13:54:32 +0300

    Final version

--- main.c
+++ main.c
@@ -11,18 +11,27 @@ int main(int argc, char** argv){
 	char sep[] = "=", sep1[] = " ";
 	if (argc<=1)
 		return 0;
+//	printf("%s\n %s", argv[1], argv[2]);
 	for (i=2; i<argc; i++) {
-		my_var = strtok(argv[i], sep);
-		val = strtok(NULL, sep);
-		setenv(my_var, val, 1);
-		printf("%s %s\n", my_var, val);
+		if (putenv(argv[i])==0) printf("OK\n");
+		fflush(stdout);
 	}
 	argv1[0] = strtok(argv[1], sep1);
 	while ((val = strtok(NULL, sep1))!=NULL) {
-		argv1[k] = val;
+		//printf("%s\n", val);
+		if (val[0]='$')
+			argv1[k] = getenv(strtok(val, "$"));
+		else
+			argv1[k] = val;
 		k++;
 	}
 	argv1[k+1] = NULL;
+	k=0;
+	while ((val = argv1[k])!=NULL){
+		k++;
+		//printf("%s\n", val);
+	}
+	fflush(stdout);
 	execvp(argv1[0], argv1);
 	return 0;
 }
